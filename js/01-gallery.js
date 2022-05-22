26f7345ba60bdc4c7e6f6dc2f8bbb90f839b205d
Siarhey Luchynovich <94983189+SiarheyLu4@users.noreply.github.com>
2022-05-22 17:55:34 +0300

    task 1

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,42 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+const galleryEl = document.querySelector(".gallery");
+
+const galleryItemsEl = galleryItems ;
+
+console.log(galleryItemsEl)
+
+const markup = galleryItemsEl.map(({ preview, original, description }) => {
+   return `<div class="gallery__item">
+  <a class="gallery__link" href="${original}" pointer-events = "none">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+  </a>
+</div>`}).join("");
+
+console.log(markup);
+
+galleryEl.insertAdjacentHTML("afterbegin", markup);
+
+// library
+
+const onGalleryClick = (e) => {
+    e.preventDefault();
+    if (!e.target.classList.contains("gallery__image")) {
+        return;
+    } else {
+    const instance = basicLightbox.create(`
+    <img src="${e.target.dataset.source}" width="800" height="600">`);
+
+        instance.show();
+    };
+    
+}
+galleryEl.addEventListener("click", onGalleryClick)
+
+// console.log(galleryItems);
