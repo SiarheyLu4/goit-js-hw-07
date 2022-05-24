cbb414ff4899f0e9000bfb89d8e250e2abcf16ff
Siarhey Luchynovich <94983189+SiarheyLu4@users.noreply.github.com>
2022-05-24 22:38:05 +0300

    task 2 corrected

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -26,17 +26,19 @@ galleryEl.insertAdjacentHTML("afterbegin", markup);
 // library
 
 const onGalleryClick = (e) => {
+
     e.preventDefault();
-    if (!e.target.classList.contains("gallery__image")) {
+  if (!e.target.classList.contains("gallery__image")) {
+    ;
         return;
     } else {
     const instance = basicLightbox.create(`
     <img src="${e.target.dataset.source}" width="800" height="600">`);
 
         instance.show();
-    };
-    
+  };
 }
 galleryEl.addEventListener("click", onGalleryClick)
+
 
 // console.log(galleryItems);
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -8,9 +8,9 @@ const galleryItemsEl = galleryItems ;
 console.log(galleryItemsEl)
 
 const markup = galleryItemsEl.map(({ preview, original, description }) => {
-   return `<a class="gallery__item" href="${original}">
+   return `<li><a class="gallery__item" href="${original}">
   <img class="gallery__image" src="${preview}" alt="${description}" />
-</a>`}).join("");
+</a></li>`}).join("");
 
 console.log(markup);
 
